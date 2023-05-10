4136984b6796509a87e28a347a2d3d386f450669
Ester Cortes <002escor@gmail.com>
2023-05-10 17:19:33 +0200

    adding moving to planets + descriptions

--- components/3d_components/Planet.js
+++ components/3d_components/Planet.js
@@ -1,36 +1,66 @@
 import { useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
-import { Html, useTexture } from "@react-three/drei";
+import { Html, useTexture, Text } from "@react-three/drei";
+import * as THREE from "three";
 import styles from "./Planet.module.css";
+import PlanetCard from "../html_components/PlanetCard";
 
-const Planet = ({ position }) => {
+const Planet = ({ position, title }) => {
 	const meshRef = useRef();
 
-	// const planetTextures = useTexture({
-	// 	map: "/rock_surface_diff_4k.jpg",
-	// 	displacementMap: "/rock_surface_disp_4k.png",
-	// 	aoMap: "/rock_surface_arm_4k.jpg",
-	// 	roughnessMap: "/rock_surface_arm_4k.jpg",
-	// 	metalnessMap: "/rock_surface_arm_4k.jpg",
-	// 	normalMap: "/rock_surface_nor_gl_4k.jpg",
-	// 	// alpha: "/alpha.png",
-	// });
+	const [planetClicked, setPlanetClicked] = useState(false);
+	const [showDescription, setShowDescription] = useState(false);
+
+	const vec = new THREE.Vector3();
+
+	useFrame((state) => {
+		if (planetClicked) {
+			state.camera.lookAt(meshRef.current.position);
+			state.camera.position.lerp(
+				vec.set(position[0], position[1] + 20, -50),
+				0.01
+			);
+			state.camera.updateProjectionMatrix();
+			console.log(state.camera.position);
+		}
+		return null;
+	});
 
 	useFrame(({ clock }) => {
 		const elapsedTime = clock.getElapsedTime();
 		const speed = 1;
 
-		if (meshRef.current) {
+		if (meshRef.current && planetClicked === false) {
 			meshRef.current.position.y =
 				position[1] + Math.sin(elapsedTime * speed) * 2;
 		}
 	});
 
 	return (
-		<mesh position={position} ref={meshRef}>
-			<sphereGeometry args={[24, 32, 16]} />
-			<meshStandardMaterial color="#571F8F" />
-		</mesh>
+		<>
+			<mesh
+				position={position}
+				ref={meshRef}
+				onClick={() => setPlanetClicked(!planetClicked)}
+			>
+				<sphereGeometry args={[11, 32, 16]} />
+				<meshStandardMaterial color="#571F8F" />
+				<Text
+					position={[0, 0, 0]}
+					scale={[4, 4, 8]}
+					color="white" // default
+					anchorX="center" // default
+					anchorY={4}
+				>
+					{title}
+				</Text>
+				{planetClicked && (
+					<Html center>
+						<PlanetCard title={title} />
+					</Html>
+				)}
+			</mesh>
+		</>
 	);
 };
 
--- components/html_components/Welcome.js
+++ components/html_components/Welcome.js
@@ -4,11 +4,23 @@ import styles from "./Welcome.module.css";
 const Welcome = () => {
 	return (
 		<section className={styles.sectionWrapper}>
-			<h1 className={styles.title}>Welcome</h1>
+			<h1 className={styles.title}>I'm Ester Cortés</h1>
+			<div className={styles.iconWrapper}>
+				<img
+					src="/ester_cortes_image.png"
+					alt="Ester Cortés in a room with pink background lightning"
+					className={styles.icon}
+				/>
+			</div>
+
 			<p className={styles.text}>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab tenetur
-				dolore, voluptates itaque nisi unde. Saepe, assumenda eaque? Modi nisi
-				sit natus eos numquam earum, quibusdam tempora. Hic, debitis possimus!
+				A frontend developer...Who spent way too long trying to make these
+				planets wobble a little, so I hope you can appreciate it.
+			</p>
+			<p className={styles.text}>
+				My goal is that you leave this website interested to see what else I can
+				work on for 8 hours uninterrupted. <br />
+				Now: take a look around!
 			</p>
 		</section>
 	);
