c9b27664c775f92b90dd6b4f76bfca6a5cb191b1
Ester Cortes <002escor@gmail.com>
2023-05-11 18:46:34 +0200

    adding about me section

--- components/html_components/planet_sections/AboutMe.js
+++ components/html_components/planet_sections/AboutMe.js
@@ -1,13 +1,27 @@
 import React from "react";
-import styles from "../PlanetCard.module.css";
+import styles from "./AboutMe.module.css";
+import information from "../../../information.json";
 
-const AboutMe = ({ title }) => {
+const AboutMe = ({ sectionTitle }) => {
 	return (
 		<>
-			<div className={styles.cardContentsWrapper}>
-				<h1 className={styles.title}>{title}</h1>
-				<p>I am a frontend developer</p>
-			</div>
+			<section className={styles.cardContentsWrapper}>
+				<h1 className={styles.title}>{sectionTitle}</h1>
+				<article className={styles.articleWrapper}>
+					<div className={styles.imageContainer}>
+						<img
+							src="/ester_cortes_image.png"
+							alt="ester cortes in a room with pink background lighting"
+						/>
+						<p>It's me!</p>
+					</div>
+					<div className={styles.descriptionsContainer}>
+						<p className={styles.description}>{information.aboutInfo[0]}</p>
+						<p className={styles.description}>{information.aboutInfo[1]}</p>
+						<p className={styles.description}>{information.aboutInfo[2]}</p>
+					</div>
+				</article>
+			</section>
 		</>
 	);
 };
